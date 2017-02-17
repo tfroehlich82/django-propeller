1a8b5d0f4a94fc1014e725eb3e7ef62e53f55915
Thorsten Fröhlich <thorsten.froehlich@metromec.ch>
2017-02-17 17:05:40 +0100

    Added dist info

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from distutils.core import setup
 setup(
     name='django-propeller',
     version='1.0.0',
-    packages=['demo.demo', 'propeller', 'propeller.migrations', 'propeller.templatetags'],
+    packages=['demo.demo', 'django_propeller', 'django_propeller.migrations', 'django_propeller.templatetags',
+              'django_propeller.static'],
     url='https://github.com/tfroehlich82/django-propeller',
     license='MIT',
     author='Thorsten Froehlich',
-    author_email='',
+    author_email='tfroehlich82@gmx.ch',
     description='Propeller integration with Django. Propeller is a front-end responsive framework based on Google\'s '
-                'Material Design Standards & Bootstrap. http://propeller.in/index.html')
+                'Material Design Standards & Bootstrap. http://propeller.in/index.html'
+)
