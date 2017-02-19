2c8de7f2f0d791596714a3ecd0f7b204dd4a88d9
Thorsten Fröhlich <t.loop@gmx.net>
2017-02-19 21:53:21 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,66 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+import django_propeller
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = django_propeller.__version__
+
+if sys.argv[-1] == 'publish':
+    os.system('cd docs && make html')
+    os.system('python setup.py sdist upload')
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+if sys.argv[-1] == 'test':
+    print("Running tests only on current environment.")
+    print("Use `tox` for testing multiple environments.")
+    os.system('python manage.py test')
+    sys.exit()
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
 
 setup(
     name='django-propeller',
-    version='1.0.0',
-    packages=['demo.demo', 'django_propeller', 'django_propeller.migrations', 'django_propeller.templatetags',
-              'django_propeller.static'],
-    url='https://github.com/tfroehlich82/django-propeller',
-    license='MIT',
+    version=version,
+    description="""Propeller support for Django projects""",
+    long_description=readme + '\n',
     author='Thorsten Froehlich',
-    author_email='tfroehlich82@gmx.ch',
-    description='Propeller integration with Django. Propeller is a front-end responsive framework based on Google\'s '
-                'Material Design Standards & Bootstrap. http://propeller.in/index.html'
+    author_email='tfroehlich82@gmx.ch   ',
+    url='https://github.com/dyve/django-propeller',
+    packages=[
+        'django_propeller',
+    ],
+    include_package_data=True,
+    install_requires=[
+    ],
+    license="MIT License",
+    zip_safe=False,
+    keywords='django-propeller',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+    ],
 )
