001f0cb9da19045614161c8b9244f90d1b0bede8
Thorsten Fröhlich <t.loop@gmx.net>
2017-02-26 13:03:15 +0100

    Improved tests

--- django_propeller/tests.py
+++ django_propeller/tests.py
@@ -898,4 +898,18 @@ class PropellerNavBarTests(TestCase):
         self.assertEqual(TestNavbar().brandname, 'propeller-test')
 
     def test_rendered_template(self):
-        pass
+        res = render_template_with_propeller(
+            '{% pmd_navbar testnav %}', {'testnav', TestNavbar()}
+        )
+        self.assertInHTML('<nav class="navbar navbar-fixed-top pmd-navbar pmd-z-depth"><div class="container-fluid">'
+                          '<!-- Brand and toggle get grouped for better mobile display --><div class="navbar-header">'
+                          '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" '
+                          'data-target="#bs-example-navbar-collapse-1" aria-expanded="false">'
+                          '<span class="sr-only">Toggle navigation</span><span class="icon-bar"></span>'
+                          '<span class="icon-bar"></span><span class="icon-bar"></span></button>'
+                          '<a href="" class="navbar-brand navbar-brand-custome"></a></div>'
+                          '<!-- Collect the nav links, forms, and other content for toggling -->'
+                          '<div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">'
+                          '<ul class="nav navbar-nav"></ul></div><!-- /.navbar-collapse --></div>'
+                          '<!-- /.container-fluid --><div class="pmd-sidebar-overlay"></div></nav>', res)
+
