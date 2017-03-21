5829f9ba86c4e774f6a5d9df734e751992e9eed7
Thorsten Fröhlich <thorsten.froehlich@metromec.ch>
2017-03-21 17:16:29 +0100

    refactored tests

---
+++ django_propeller/test_results.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+
+"""Provides expected test results"""
+
+RESULT_CARD1 = '<div class="pmd-card pmd-card-default pmd-z-depth col-md-4">' \
+               '<div class="pmd-card-title">' \
+               '<div class="media-left">' \
+               '<img src="http://propeller.in/assets/images/avatar-icon-40x40.png" width="40" height="40">' \
+               '</div>' \
+               '<div class="media-body media-middle">' \
+               '<h3 class="pmd-card-title-text">Two-line item</h3>' \
+               '<span class="pmd-card-subtitle-text">Secondary text</span>' \
+               '</div>' \
+               '</div>' \
+               '<img src="http://propeller.in/assets/images/profile-pic.png" class="img-responsive">' \
+               '<div class="pmd-card-title">' \
+               '<h2 class="pmd-card-title-text">Title goes here</h2>' \
+               '<span class="pmd-card-subtitle-text">Secondary text</span>' \
+               '</div>' \
+               '<div class="pmd-card-body">' \
+               'Cards provide context and an entry point to more robust information and views. ' \
+               'Don\'t overload cards with extraneous information or actions.' \
+               '</div>' \
+               '<div class="pmd-card-actions">' \
+               '<button class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" type="button">' \
+               '<i class="material-icons pmd-sm">share</i>' \
+               '</button>' \
+               '<button class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" type="button">' \
+               '<i class="material-icons pmd-sm">thumb_up</i>' \
+               '</button>' \
+               '<button class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" type="button">' \
+               '<i class="material-icons pmd-sm">drafts</i>' \
+               '</button>' \
+               '</div>' \
+               '<div class="pmd-card-actions">' \
+               '<button class="btn btn-primary pmd-ripple-effect pmd-btn-default" href="#" type="button">primary' \
+               '</button>' \
+               '<button class="btn btn-default pmd-ripple-effect pmd-btn-default" href="#" type="button">Action' \
+               '</button>' \
+               '<button class="btn btn-default pmd-ripple-effect pmd-btn-default" href="#" type="button">third' \
+               '</button>' \
+               '</div>' \
+               '</div>'
+
+RESULT_CARD2 = '<div class="pmd-card pmd-card-default pmd-z-depth col-md-4">' \
+               '<img src="http://propeller.in/assets/images/profile-pic.png" class="img-responsive">' \
+               '<div class="pmd-card-title">' \
+               '<h2 class="pmd-card-title-text">Title goes here</h2>' \
+               '<span class="pmd-card-subtitle-text">Secondary text</span>' \
+               '</div>' \
+               '<div class="pmd-card-actions">' \
+               '<button class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" type="button">' \
+               '<i class="material-icons pmd-sm">share</i>' \
+               '</button>' \
+               '<button class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" type="button">' \
+               '<i class="material-icons pmd-sm">thumb_up</i>' \
+               '</button>' \
+               '<button class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" type="button">' \
+               '<i class="material-icons pmd-sm">drafts</i>' \
+               '</button>' \
+               '</div>' \
+               '</div>'
+
+RESULT_CARD3 = '<div class="pmd-card pmd-card-inverse pmd-z-depth col-md-4">' \
+               '<div class="pmd-card-title">' \
+               '<div class="media-left">' \
+               '<img src="http://propeller.in/assets/images/avatar-icon-40x40.png" width="40" height="40">' \
+               '</div>' \
+               '<div class="media-body media-middle">' \
+               '<h3 class="pmd-card-title-text">Two-line item</h3>' \
+               '<span class="pmd-card-subtitle-text">Secondary text</span>' \
+               '</div>' \
+               '</div>' \
+               '<img src="http://propeller.in/assets/images/profile-pic.png" class="img-responsive">' \
+               '<div class="pmd-card-title">' \
+               '<h2 class="pmd-card-title-text">Title goes here</h2>' \
+               '<span class="pmd-card-subtitle-text">Secondary text</span>' \
+               '</div>' \
+               '<div class="pmd-card-body">' \
+               'Cards provide context and an entry point to more robust information and views. ' \
+               'Don\'t overload cards with extraneous information or actions.' \
+               '</div>' \
+               '<div class="pmd-card-actions">' \
+               '<button class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" type="button">' \
+               '<i class="material-icons pmd-sm">share</i>' \
+               '</button>' \
+               '<button class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" type="button">' \
+               '<i class="material-icons pmd-sm">thumb_up</i>' \
+               '</button>' \
+               '<button class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" type="button">' \
+               '<i class="material-icons pmd-sm">drafts</i>' \
+               '</button>' \
+               '</div>' \
+               '<div class="pmd-card-actions">' \
+               '<button class="btn btn-primary pmd-ripple-effect pmd-btn-default" href="#" type="button">primary' \
+               '</button>' \
+               '<button class="btn btn-default pmd-ripple-effect pmd-btn-default" href="#" type="button">Action' \
+               '</button>' \
+               '<button class="btn btn-default pmd-ripple-effect pmd-btn-default" href="#" type="button">third' \
+               '</button>' \
+               '</div>' \
+               '</div>'
+
+RESULT_CARD4 = '<div class="pmd-card pmd-card-media-inline pmd-card-default pmd-z-depth col-md-4">' \
+               '<img src="http://propeller.in/assets/images/profile-pic.png" width="80" height="80">' \
+               '<div class="pmd-card-actions">' \
+               '<button class="btn btn-primary pmd-ripple-effect pmd-btn-default" href="#" type="button">primary' \
+               '</button>' \
+               '<button class="btn btn-default pmd-ripple-effect pmd-btn-default" href="#" type="button">Action' \
+               '</button>' \
+               '<button class="btn btn-default pmd-ripple-effect pmd-btn-default" href="#" type="button">third' \
+               '</button>' \
+               '</div>' \
+               '</div>'
+
+RESULT_NAVBAR1 = '<nav class="navbar navbar-fixed-top pmd-navbar pmd-z-depth">' \
+                 '<div class="container-fluid">' \
+                 '<div class="navbar-header">' \
+                 '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" ' \
+                 'data-target="#bs-example-navbar-collapse-1" aria-expanded="false">' \
+                 '<span class="sr-only">Toggle navigation</span>' \
+                 '<span class="icon-bar"></span>' \
+                 '<span class="icon-bar"></span>' \
+                 '<span class="icon-bar"></span>' \
+                 '</button>' \
+                 '<a href="" class="navbar-brand navbar-brand-custome"></a>' \
+                 '</div>' \
+                 '<div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">' \
+                 '<ul class="nav navbar-nav"></ul>' \
+                 '</div><!-- /.navbar-collapse --></div>' \
+                 '<!-- /.container-fluid -->' \
+                 '<div class="pmd-sidebar-overlay">' \
+                 '</div>' \
+                 '</nav>'
+
+RESULT_NAVBAR2 = '<nav class="navbar navbar-fixed-top pmd-navbar pmd-z-depth">' \
+                 '<div class="container-fluid">' \
+                 '<!-- Brand and toggle get grouped for better mobile display -->' \
+                 '<div class="navbar-header">' \
+                 '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" ' \
+                 'data-target="#bs-example-navbar-collapse-1" aria-expanded="false">' \
+                 '<span class="sr-only">Toggle navigation</span>' \
+                 '<span class="icon-bar"></span>' \
+                 '<span class="icon-bar"></span>' \
+                 '<span class="icon-bar"></span>' \
+                 '</button>' \
+                 '<a href="" class="navbar-brand navbar-brand-custome"></a></div>' \
+                 '<!-- Collect the nav links, forms, and other content for toggling -->' \
+                 '<div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">' \
+                 '<ul class="nav navbar-nav"></ul>' \
+                 '</div><!-- /.navbar-collapse --></div>' \
+                 '<!-- /.container-fluid -->' \
+                 '<div class="pmd-sidebar-overlay">' \
+                 '</div>' \
+                 '</nav>'
+
+RESULT_NAVBAR3 = '<nav class="navbar navbar-fixed-top pmd-navbar pmd-z-depth">' \
+                 '<div class="container-fluid">' \
+                 '<!-- Brand and toggle get grouped for better mobile display -->' \
+                 '<div class="navbar-header">' \
+                 '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" ' \
+                 'data-target="#bs-example-navbar-collapse-1" aria-expanded="false">' \
+                 '<span class="sr-only">Toggle navigation</span>' \
+                 '<span class="icon-bar"></span>' \
+                 '<span class="icon-bar"></span>' \
+                 '<span class="icon-bar"></span>' \
+                 '</button>' \
+                 '<a href="" class="navbar-brand navbar-brand-custome"></a></div>' \
+                 '<!-- Collect the nav links, forms, and other content for toggling -->' \
+                 '<div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">' \
+                 '<ul class="nav navbar-nav"></ul>' \
+                 '</div><!-- /.navbar-collapse --></div>' \
+                 '<!-- /.container-fluid -->' \
+                 '<div class="pmd-sidebar-overlay">' \
+                 '</div>' \
+                 '</nav>'
--- django_propeller/tests.py
+++ django_propeller/tests.py
@@ -16,6 +16,8 @@ from .text import text_value, text_concat
 from .utils import add_css_class, render_tag
 from .test_data import DemoCard1, DemoCard2, DemoCard3, DemoCard4, TestNavbar1, TestNavbar2, TestNavbar3, \
     NavBarLinkItem, NavBarDropDownDivider, NavBarDropDownItem, NavBar
+from .test_results import RESULT_CARD1, RESULT_CARD2, RESULT_CARD3, RESULT_CARD4, RESULT_NAVBAR1, RESULT_NAVBAR2, \
+    RESULT_NAVBAR3
 
 try:
     from html.parser import HTMLParser
@@ -869,45 +871,15 @@ class PropellerNavBarTests(TestCase):
         res = self.render_template_with_propeller(
             '{% propeller_navbar testnav %}', {'testnav', TestNavbar1()}
         )
-        self.assertInHTML('<nav class="navbar navbar-fixed-top pmd-navbar pmd-z-depth"><div class="container-fluid">'
-                          '<!-- Brand and toggle get grouped for better mobile display --><div class="navbar-header">'
-                          '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" '
-                          'data-target="#bs-example-navbar-collapse-1" aria-expanded="false">'
-                          '<span class="sr-only">Toggle navigation</span><span class="icon-bar"></span>'
-                          '<span class="icon-bar"></span><span class="icon-bar"></span></button>'
-                          '<a href="" class="navbar-brand navbar-brand-custome"></a></div>'
-                          '<!-- Collect the nav links, forms, and other content for toggling -->'
-                          '<div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">'
-                          '<ul class="nav navbar-nav"></ul></div><!-- /.navbar-collapse --></div>'
-                          '<!-- /.container-fluid --><div class="pmd-sidebar-overlay"></div></nav>', res)
+        self.assertInHTML(RESULT_NAVBAR1, res)
         res = self.render_template_with_propeller(
             '{% propeller_navbar testnav %}', {'testnav', TestNavbar2()}
         )
-        self.assertInHTML('<nav class="navbar navbar-fixed-top pmd-navbar pmd-z-depth"><div class="container-fluid">'
-                          '<!-- Brand and toggle get grouped for better mobile display --><div class="navbar-header">'
-                          '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" '
-                          'data-target="#bs-example-navbar-collapse-1" aria-expanded="false">'
-                          '<span class="sr-only">Toggle navigation</span><span class="icon-bar"></span>'
-                          '<span class="icon-bar"></span><span class="icon-bar"></span></button>'
-                          '<a href="" class="navbar-brand navbar-brand-custome"></a></div>'
-                          '<!-- Collect the nav links, forms, and other content for toggling -->'
-                          '<div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">'
-                          '<ul class="nav navbar-nav"></ul></div><!-- /.navbar-collapse --></div>'
-                          '<!-- /.container-fluid --><div class="pmd-sidebar-overlay"></div></nav>', res)
+        self.assertInHTML(RESULT_NAVBAR2, res)
         res = self.render_template_with_propeller(
             '{% propeller_navbar testnav %}', {'testnav', TestNavbar3()}
         )
-        self.assertInHTML('<nav class="navbar navbar-fixed-top pmd-navbar pmd-z-depth"><div class="container-fluid">'
-                          '<!-- Brand and toggle get grouped for better mobile display --><div class="navbar-header">'
-                          '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" '
-                          'data-target="#bs-example-navbar-collapse-1" aria-expanded="false">'
-                          '<span class="sr-only">Toggle navigation</span><span class="icon-bar"></span>'
-                          '<span class="icon-bar"></span><span class="icon-bar"></span></button>'
-                          '<a href="" class="navbar-brand navbar-brand-custome"></a></div>'
-                          '<!-- Collect the nav links, forms, and other content for toggling -->'
-                          '<div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">'
-                          '<ul class="nav navbar-nav"></ul></div><!-- /.navbar-collapse --></div>'
-                          '<!-- /.container-fluid --><div class="pmd-sidebar-overlay"></div></nav>', res)
+        self.assertInHTML(RESULT_NAVBAR3, res)
 
     def test_navbar_items(self):
         for itm in TestNavbar1().items:
@@ -925,75 +897,20 @@ class PropellerNavBarTests(TestCase):
 class PropellerCardTests(TestCase):
     def test_rendered_template(self):
         res = self.render_template_with_propeller(
-            '{% propeller_card card1 %}', {'card1': DemoCard1()}
+            '{% propeller_card card1 %}',
+            context={'card1': DemoCard1()}
         )
-        self.assertInHTML('<div class="pmd-card pmd-card-default pmd-z-depth col-md-4"><div class="pmd-card-title">'
-                          '<div class="media-left"><img src="http://propeller.in/assets/images/avatar-icon-40x40.png" '
-                          'width="40" height="40"></div><div class="media-body media-middle">'
-                          '<h3 class="pmd-card-title-text">Two-line item</h3>'
-                          '<span class="pmd-card-subtitle-text">Secondary text</span></div></div>'
-                          '<img src="http://propeller.in/assets/images/profile-pic.png" class="img-responsive">'
-                          '<div class="pmd-card-title"><h2 class="pmd-card-title-text">Title goes here</h2>'
-                          '<span class="pmd-card-subtitle-text">Secondary text</span></div><div class="pmd-card-body">'
-                          'Cards provide context and an entry point to more robust information and views. '
-                          'Don\'t overload cards with extraneous information or actions.</div>'
-                          '<div class="pmd-card-actions"><button class="btn btn-sm pmd-btn-fab pmd-btn-flat '
-                          'pmd-ripple-effect btn-primary" type="button"><i class="material-icons pmd-sm">share</i>'
-                          '</button><button class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" '
-                          'type="button"><i class="material-icons pmd-sm">thumb_up</i></button><button '
-                          'class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" type="button"><i '
-                          'class="material-icons pmd-sm">drafts</i></button></div><div class="pmd-card-actions">'
-                          '<button class="btn btn-primary pmd-ripple-effect pmd-btn-default" href="#" type="button">'
-                          'primary</button><button class="btn btn-default pmd-ripple-effect pmd-btn-default" href="#" '
-                          'type="button">Action</button><button class="btn btn-default pmd-ripple-effect '
-                          'pmd-btn-default" href="#" type="button">third</button></div></div>',
-                          res)
+        self.assertInHTML(RESULT_CARD1, res)
         res = self.render_template_with_propeller(
-            '{% propeller_card card2 %}', {'card2': DemoCard2()}
+            '{% propeller_card card2 %}',
+            context={'card2': DemoCard2()}
         )
-        self.assertInHTML('<div class="pmd-card pmd-card-default pmd-z-depth col-md-4">'
-                          '<img src="http://propeller.in/assets/images/profile-pic.png" class="img-responsive">'
-                          '<div class="pmd-card-title"><h2 class="pmd-card-title-text">Title goes here</h2>'
-                          '<span class="pmd-card-subtitle-text">Secondary text</span></div>'
-                          '<div class="pmd-card-actions"><button class="btn btn-sm pmd-btn-fab pmd-btn-flat '
-                          'pmd-ripple-effect btn-primary" type="button"><i class="material-icons pmd-sm">share</i>'
-                          '</button><button class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" '
-                          'type="button"><i class="material-icons pmd-sm">thumb_up</i></button><button '
-                          'class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" type="button">'
-                          '<i class="material-icons pmd-sm">drafts</i></button></div></div>',
-                          res)
+        self.assertInHTML(RESULT_CARD2, res)
         res = self.render_template_with_propeller(
             '{% propeller_card card3 %}', {'card3': DemoCard3()}
         )
-        self.assertInHTML('<div class="pmd-card pmd-card-inverse pmd-z-depth col-md-4"><div class="pmd-card-title">'
-                          '<div class="media-left"><img src="http://propeller.in/assets/images/avatar-icon-40x40.png" '
-                          'width="40" height="40"></div><div class="media-body media-middle"><h3 '
-                          'class="pmd-card-title-text">Two-line item</h3><span class="pmd-card-subtitle-text">'
-                          'Secondary text</span></div></div>'
-                          '<img src="http://propeller.in/assets/images/profile-pic.png" class="img-responsive">'
-                          '<div class="pmd-card-title"><h2 class="pmd-card-title-text">Title goes here</h2>'
-                          '<span class="pmd-card-subtitle-text">Secondary text</span></div><div class="pmd-card-body">'
-                          'Cards provide context and an entry point to more robust information and views. '
-                          'Don\'t overload cards with extraneous information or actions.</div><div '
-                          'class="pmd-card-actions"><button class="btn btn-sm pmd-btn-fab pmd-btn-flat '
-                          'pmd-ripple-effect btn-primary" type="button"><i class="material-icons pmd-sm">share</i>'
-                          '</button><button class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" '
-                          'type="button"><i class="material-icons pmd-sm">thumb_up</i></button><button '
-                          'class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" type="button">'
-                          '<i class="material-icons pmd-sm">drafts</i></button></div><div class="pmd-card-actions">'
-                          '<button class="btn btn-primary pmd-ripple-effect pmd-btn-default" href="#" type="button">'
-                          'primary</button><button class="btn btn-default pmd-ripple-effect pmd-btn-default" href="#" '
-                          'type="button">Action</button><button class="btn btn-default pmd-ripple-effect '
-                          'pmd-btn-default" href="#" type="button">third</button></div></div>',
-                          res)
+        self.assertInHTML(RESULT_CARD3, res)
         res = self.render_template_with_propeller(
             '{% propeller_card card4 %}', {'card4': DemoCard4()}
         )
-        self.assertInHTML('<div class="pmd-card pmd-card-media-inline pmd-card-default pmd-z-depth col-md-4">'
-                          '<img src="http://propeller.in/assets/images/profile-pic.png" width="80" height="80">'
-                          '<div class="pmd-card-actions"><button class="btn btn-primary pmd-ripple-effect '
-                          'pmd-btn-default" href="#" type="button">primary</button><button class="btn btn-default '
-                          'pmd-ripple-effect pmd-btn-default" href="#" type="button">Action</button><button '
-                          'class="btn btn-default pmd-ripple-effect pmd-btn-default" href="#" type="button">third'
-                          '</button></div></div>',
-                          res)
+        self.assertInHTML(RESULT_CARD4, res)
